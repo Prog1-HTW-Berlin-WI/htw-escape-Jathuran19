46cd7cd74c3c6da8e6266725895e487381483fbd
Jathuran19 <[200~>
2026-01-07 14:58:49 +0100

    Javadoc und Athor-Tags ergänzt

--- app/EscapeApp.java
+++ app/EscapeApp.java
@@ -1,4 +1,4 @@
-package app;
+
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -7,12 +7,20 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Scanner;
 
+/**
+ * Klasse für Hauptprogrammablauf des Spiels
+ * @author Jathuran Sathananthan
+ * @author Arda Bingöl 
+ */
 public class EscapeApp {
 
     public static final String SAVE_FILE_NAME = "save";
     private EscapeGame game;
     private boolean gameRunning = true;
 
+    /**
+     * Spielstart und je nach Wahl wird man zum Menü weitergeleitet 
+     */
     public static void main(String[] args) {
         System.out.println("Welcome to the HTW escape");
         System.out.println("========================================\n");
@@ -26,7 +34,9 @@ public class EscapeApp {
             System.out.println("====================");
         }
     }
-
+    /**
+     * zeigt Menüansicht an 
+     */
     private void showMainMenu() {
         System.out.println("You're in the main menu");
         System.out.println("What do you want to do next?");
@@ -35,14 +45,20 @@ public class EscapeApp {
         System.out.println("");
         System.out.println("Please choose a number between 1 and 6: ");
     }
-
+    /**
+     * User führt eine Eingabe aus 
+     * @return Benutzereingabe als String
+     */
     private String readUserInput() {
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
         // TBD
         return userInput;
     }
-
+    /**
+     * Fehlereingabe wird dem Benutzer gemeldet 
+     * @param input
+     */
     private void handleUserInput(String input) {
         switch (input) {
             case "1":
@@ -58,23 +74,32 @@ public class EscapeApp {
                 break;
         }
     }
-
+    /**
+     * Startet neues Spiel
+     */
     private void startGame() {
         this.game = new EscapeGame();
         resumeGame();
     }
-
+    /**
+     * Spielstand wird fortgesetzt 
+     */
     private void resumeGame() {
         this.game.setGameRunning(true);
         this.game.run();
     }
-
+    /**
+     * Spielstand wird gelöscht
+     */
     private void deleteGame() {
         if (new File(SAVE_FILE_NAME).delete()) {
             System.out.println("Game deleted!");
         }
     }
-
+    /**
+     * Spielstand wird gespeichert
+     * @return Spiel wird gespeichert
+     */
     private void saveGame() {
         try (FileOutputStream fos = new FileOutputStream(SAVE_FILE_NAME);
              ObjectOutputStream oos = new ObjectOutputStream(fos)) {
@@ -86,7 +111,9 @@ public class EscapeApp {
         }
         System.out.println("Game saved!");
     }
-
+    /**
+     * Spiel wird geladen 
+     */
     private void loadGame() {
         try (FileInputStream fis = new FileInputStream(SAVE_FILE_NAME);
              ObjectInputStream ois = new ObjectInputStream(fis)) {
@@ -96,15 +123,24 @@ public class EscapeApp {
             System.err.println("Something went wrong while loading the game: " + ex.getMessage());
         }
     }
-
+    /**
+     * Spiel läuft 
+     * @return Spiel vorhanden true, nicht vorhanden false
+     */
     private boolean isGameRunning() {
         return game != null;
     }
-
+    /**
+     * Spiel beendet 
+     * @return true, wenn das Spiel beendet ist
+     */
     private boolean isGameFinished() {
         return game != null && game.isGameFinished();
     }
-
+    /**
+     * Prüft Dateibestand 
+     * @return true wenn etwas vorhanden ist 
+     */
     private boolean hasSavedGame() {
         return new File(SAVE_FILE_NAME).exists();
     }
--- app/EscapeGame.java
+++ app/EscapeGame.java
@@ -1,38 +1,62 @@
-package app;
+
 
 import model.Hero;
 import model.HTWRoom;
-
+/**
+ * Steuert den Ablauf und Zustand des Spiels
+ * @author Jathuran Sathananthan 
+ * @author Arda Bingöl
+ */
 public class EscapeGame {
     private final Hero hero;
     private final HTWRoom[] rooms = new HTWRoom[3];
     private boolean gameRunning = true;
     private boolean gameFinished = false;
 
+    /**
+     * Neue Spieler wird erstellt
+     */
     public EscapeGame() {
         this.hero = new Hero();
     }
-
+    /**
+     * Gibt an ob, das Spiel aktuell läuft 
+     * @return true, wenn Spiel läuft, false wenn spiel nicht läuft
+     */
     public boolean isGameRunning() {
         return gameRunning;
     }
-
+    /**
+     * Spiel wird ausgeführt 
+     * @param gameRunning
+     */
     public void setGameRunning(boolean gameRunning) {
         this.gameRunning = gameRunning;
     }
-
+    /**
+     * Gibt Spielstand an 
+     * @return true, wenn Spiel läuft, false , wenn Spiel beendet ist
+     */
     public boolean isGameFinished() {
         return gameFinished;
     }
-
+    /**
+     * Spiel wird geschlossen 
+     * @param gameFinished
+     */
     public void setGameFinished(boolean gameFinished) {
         this.gameFinished = gameFinished;
     }
-
+    /**
+     * Spiel fragt nach Aktivität
+     */
     public void run() {
         System.out.println("The game has started. Or not?");
     }
-
+    /**
+     * Gibt aktuellen Charakter zurück 
+     * @return aktuelle Charakter 
+     */
     public Hero getHero() {
         return hero;
     }
